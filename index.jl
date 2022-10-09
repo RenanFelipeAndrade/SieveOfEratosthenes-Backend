8ba4c2d2d03b15e53fbc76a1f26435633baa00bb
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-09 16:47:14 -0300

    Altered floor to fld; Removed for, more performant

--- index.jl
+++ index.jl
@@ -1,11 +1,11 @@
 using BenchmarkTools
 function main()
-  maxRange = 10000000
-  sieveVec = Vector{UInt128}()
+  maxRange::UInt128 = 100000000
   calc::UInt128 = 2^128 - uint128"1"
-  for _ = 1:floor(maxRange / 128)+1
-    push!(sieveVec, calc)
-  end
+  # initializes a vector of n size 
+  sieveVec = Vector{UInt128}(undef, fld(maxRange, 128) + 1)
+  fill!(sieveVec, calc)
+
   sieveOfEratosthenes(uint128"1", sieveVec, maxRange)
 end
 
@@ -13,18 +13,18 @@ end
 Calculate primes from 0 to maxRange
 """
 function sieveOfEratosthenes(number::UInt128, sieve::Vector{UInt128}, maxRange)
-  rest::UInt128 = number % 128
-  intDiv::UInt128 = floor(number / 128) + 1
+  rest::UInt128 = (number % 128) - 1
+  intDiv = fld(number, 128) + 1
   shift::UInt128 = (uint128"1" << rest)
-  result = sieve[intDiv] & shift
+  result::UInt128 = sieve[intDiv] & shift
 
   if number <= 1
     sieve[intDiv] = xor(sieve[intDiv], shift)
   elseif result > 1
     composite::UInt128 = number * 2
     while composite < length(sieve) * 128
-      intDiv = floor(composite / 128) + 1
-      rest = composite % 128
+      intDiv = fld(composite, 128) + 1
+      rest = (composite % 128) - 1
       shift = (uint128"1" << rest)
       sieve[intDiv] = sieve[intDiv] & ~shift
       composite += number
