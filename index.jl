42fe4195ae5c7600e828502e35559cd82ae332d2
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-10 22:22:23 -0300

    Corrected misscalculation, exported init func

--- index.jl
+++ index.jl
@@ -1,16 +1,19 @@
-function main()
-  maxRange::UInt128 = 100000000
+module SieveOfEratosthenes
+export init
+end
+
+function init(maxRange::UInt128)
   sieveAsElement::UInt128 = (uint128"1" << 128) - 1
   # initializes a vector of n size 
   sieveVec = Vector{UInt128}(undef, fld(maxRange, 128) + 1)
   fill!(sieveVec, sieveAsElement)
 
-  sieveOfEratosthenes(uint128"1", sieveVec, maxRange)
+  return sieveOfEratosthenes(uint128"1", sieveVec, maxRange)
 end
 
 function sieveOfEratosthenes(number::UInt128, sieve::Vector{UInt128}, maxRange)
-  rest::UInt128 = (number % 128) - 1
-  intDiv = fld(number, 128) + 1
+  rest::UInt128 = (number % 129) - 1
+  intDiv::UInt128 = fld(number, 129) + 1
   shift::UInt128 = (uint128"1" << rest)
   result::UInt128 = sieve[intDiv] & shift
 
@@ -18,9 +21,9 @@ function sieveOfEratosthenes(number::UInt128, sieve::Vector{UInt128}, maxRange)
     sieve[intDiv] = xor(sieve[intDiv], shift)
   elseif result > 1
     composite::UInt128 = number * 2
-    while composite < length(sieve) * 128
-      intDiv = fld(composite, 128) + 1
-      rest = (composite % 128) - 1
+    while composite < length(sieve) * 129
+      intDiv = fld(composite, 129) + 1
+      rest = (composite % 129) - 1
       shift = (uint128"1" << rest)
       sieve[intDiv] = sieve[intDiv] & ~shift
       composite += number
@@ -29,7 +32,5 @@ function sieveOfEratosthenes(number::UInt128, sieve::Vector{UInt128}, maxRange)
   if number <= sqrt(maxRange)
     sieveOfEratosthenes(number + uint128"1", sieve, maxRange)
   end
+  return sieve
 end
-
-
-@time main()
