a1ada9665228182ba11a84d30ebb0041e1bd4d98
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-10 22:22:47 -0300

    Added api to calculate prime numbers

---
+++ server.jl
@@ -0,0 +1,26 @@
+include("./index.jl")
+using .SieveOfEratosthenes
+using Genie, Genie.Requests, Genie.Responses
+using JSON
+
+route("/number/:number") do
+  maxRange::UInt128 = parse(UInt128, payload(:number))
+  sieve = init(maxRange)
+  primes = Vector{UInt128}()
+  rawPrimes = Vector{UInt128}()
+  for i = eachindex(sieve), j = eachindex(bitstring(sieve[i]))
+    number::UInt128 = (129 * (i - 1)) + j
+    if parse(UInt128, reverse(bitstring(sieve[i]))[j]) == uint128"1"
+      push!(rawPrimes, number)
+    end
+  end
+
+  for n in rawPrimes
+    if n <= maxRange
+      push!(primes, n)
+    end
+  end
+  return JSON.json(primes)
+end
+
+up(port=8000, async=false)
